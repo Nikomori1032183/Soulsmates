760736a80c0f5d9f9ea92ab30ac270598644c51b
Shannie <145741715+ShannieU@users.noreply.github.com>
2024-01-27 04:16:56 +0800

    SHane#1

--- Soulsmates/Assets/Scripts/PlayerInteractor.cs
+++ Soulsmates/Assets/Scripts/PlayerInteractor.cs
@@ -10,88 +10,47 @@ public class PlayerInteractor : MonoBehaviour
     GameObject player;
     UnityEvent onInteract;
     CollectableItem interactable;
+    CharacterData charData;
     //[SerializeField] Animator doorAnimator;
 
     public bool keyCollected = false;
     public bool locked = true;
     public int range = 4;
 
-
-
-    private void Start()
-    {
-        
-    }
-
     void Update()
     {
-        //Player interacts with objects by entering radius and clicking interact
-
-
-        
-
-
-        //float distance = Vector3.Distance(GetComponent<CollectableItem>().gameObject.transform.position, transform.position);
-
-        /*if(distance >= range)
-        {
-            Debug.Log("Collectable Object In Range");
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                interactable.onInteract.Invoke();
-            }
-        }*/
-        
-
-        //Player interacts with door by collecting key and interacting with the door
-
-        /*
-        player = GameObject.FindGameObjectWithTag("Player");
-
-        float distance = Vector3.Distance(player.transform.position, transform.position);
-
-        if (!locked && distance <= range && doorAnimator.GetBool("Close"))
-        {
-            doorAnimator.SetBool("Close", false);
-            doorAnimator.SetBool("Open", true);
-
-        }
-        if (!locked && distance >= range && doorAnimator.GetBool("Open"))
-        {
-            doorAnimator.SetBool("Open", false);
-            doorAnimator.SetBool("Close", true);
-        }
-        */
+        //check left hand for item
+        //if not add into left hand
+        //check right hand
+        //if not add into right hand
+        //else debug cant pick up
     }
 
     private void OnTriggerEnter(Collider other)
     {
         CollectableItem collect = other.GetComponent<CollectableItem>();
-
+        Debug.Log("Collectable Object In Range");
         if (collect != null)
         {
-            Debug.Log("not null");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (charData.GetLeftItem() == null)
+                {
+                    charData.SetLeftItem(collect.item);
+                    Debug.Log(collect.item.itemName + "was added to inventory");
+                }
+                else if (charData.GetRightItem() == null)
+                {
+                    charData.SetRightItem(collect.item);
+                    Debug.Log(collect.item.itemName + "was added to inventory");
+                }
+                else
+                {
+                    Debug.Log("Hands are full, cannot pick up item");
+                }
+            }
         }
-        else
-        {
-            Debug.Log("null");
-        }
-        
     }
 
-    /*
-    public void Unlock()
-    {
-        if (keyCollected)
-        {
-            locked = false;
-            Debug.Log("Door Unlocked"); //replace with pop up
-        }
-        else
-        {
-            Debug.Log("Key Needed"); //replace with pop up
-        }
-    }
-    */
 
 }
