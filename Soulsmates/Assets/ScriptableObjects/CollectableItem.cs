8cf5413dcc11bdcae025eff84daed27652195abe
Shannie <145741715+ShannieU@users.noreply.github.com>
2024-01-27 01:36:21 +0800

    Interactor & Item Class

---
+++ Soulsmates/Assets/ScriptableObjects/CollectableItem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class CollectableItem : ScriptableObject
+{
+    public string itemName;
+    public int ID;
+
+    public interface InteractableInterface
+    {
+        //public bool Interact(Interactor interactor);
+    }
+}
---
+++ Soulsmates/Assets/Scripts/PlayerInteractor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractor : MonoBehaviour
+{
+
+    //Player interacts with objects by entering radius and clicking interact
+    //
+
+    //Player interacts with door by collecting key and interacting with the door
+
+}
