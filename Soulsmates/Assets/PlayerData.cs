0ab229b4cb11eeaa7f9399cd6447194801657f2d
Nikomori1032183 <147117353+Nikomori1032183@users.noreply.github.com>
2024-01-26 23:44:48 +0800

    Im Scared

---
+++ Soulsmates/Assets/PlayerData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Author: Frank Manford
+// Description: Class for storing player data
+
+public class PlayerData : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
