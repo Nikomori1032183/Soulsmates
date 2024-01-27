a371c97dd10a44716b2282946a7c1d93f0f74012
Nikomori1032183 <147117353+Nikomori1032183@users.noreply.github.com>
2024-01-27 22:38:31 +0800

    Small Changes

--- Soulsmates/Assets/Scripts/Lover.cs
+++ Soulsmates/Assets/Scripts/Lover.cs
@@ -1,21 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+
+// Author: Frank Manford
+// Description: Class for holding Lover information
 
 public class Lover : MonoBehaviour
 {
-    // name
-    // portrait
-    // liked item
-    // liked location
+    string name;
+    Sprite sprite;
+    Item likedItem, dislikedItem;
+    Location likedLocation;
+    People dislikedPerson;
 
-    void Start()
+    public enum Location
+    {
+        Stairwell, Gym, Janitors_Closet, Cafeteria, Music_Room, Cooking_Room
+    }
+
+    public void RandomizePreferences()
+    {
+        //Random.Range(1, )
+    }
+
+    private void Start()
     {
         
     }
 
-    void Update()
-    {
-        
-    }
+    //public void LoadItems()
+    //{
+    //    string[] guids;
+    //    guids = AssetDatabase.FindAssets("t:Item", new[] { itemDirectory });
+    //    foreach (string guid in guids)
+    //    {
+    //        Debug.Log("Item: " + AssetDatabase.GUIDToAssetPath(guid));
+    //        Item item = AssetDatabase.LoadAssetAtPath<Item>(AssetDatabase.GUIDToAssetPath(guid));
+    //        items.Add(item);
+    //    }
+    //}
+
+    //public void LoadItems()
+    //{
+    //    string[] guids;
+    //    guids = AssetDatabase.FindAssets("t:Item", new[] { itemDirectory });
+    //    foreach (string guid in guids)
+    //    {
+    //        Debug.Log("Item: " + AssetDatabase.GUIDToAssetPath(guid));
+    //        Item item = AssetDatabase.LoadAssetAtPath<Item>(AssetDatabase.GUIDToAssetPath(guid));
+    //        items.Add(item);
+    //    }
+    //}
 }
