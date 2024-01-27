f5d03bfd02a4350c2648504e0d07207ce493512a
Nikomori1032183 <147117353+Nikomori1032183@users.noreply.github.com>
2024-01-27 18:59:08 +0800

    Turn System Base

--- Soulsmates/Assets/Scripts/TurnHandler.cs
+++ Soulsmates/Assets/Scripts/TurnHandler.cs
@@ -40,21 +40,25 @@ public class TurnHandler : MonoBehaviour
             case Player.Player_1:
                 currentPlayer = Player.Player_2;
                 OnTurnChange?.Invoke(player2);
+                Debug.Log("Player 2's Turn");
                 break;
 
             case Player.Player_2:
                 currentPlayer = Player.Player_3;
                 OnTurnChange?.Invoke(player3);
+                Debug.Log("Player 3's Turn");
                 break;
 
             case Player.Player_3:
                 currentPlayer = Player.Player_4;
                 OnTurnChange?.Invoke(player4);
+                Debug.Log("Player 4's Turn");
                 break;
 
             case Player.Player_4:
                 currentPlayer = Player.Player_1;
                 OnTurnChange?.Invoke(player1);
+                Debug.Log("Player 1's Turn");
                 break;
         }
     }
